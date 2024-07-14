7ab89e6bfa081fd5d0a9f075b29e6c429a0290de
EdogiStar <57968062+salihuabubakar@users.noreply.github.com>
2024-07-14 14:00:49 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={`G-${process.env.GID}`} />
     </html>
   );
 }
